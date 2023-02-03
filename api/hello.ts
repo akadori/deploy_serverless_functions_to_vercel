ab0b2139c820dbca0f879d0e44efecfebd43800e
tamaritamari <shuhei.tamari@cureapp.jp>
2023-02-03 23:02:00 +0900

    fix

--- api/hello.ts
+++ api/hello.ts
@@ -2,8 +2,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default function (req: NowRequest, res: NowResponse) {
-  // eslint-disable-next-line no-console
-  console.log(`req: ${JSON.stringify(req)}`);
   const { name = "World" } = req.query;
   res.send(`Hello ${name}!`);
 }
