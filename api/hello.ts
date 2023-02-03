2d04a73fd0c29565c565d614585f30fa6b25ff76
tamaritamari <shuhei.tamari@cureapp.jp>
2023-02-03 23:39:17 +0900

    気になる

--- api/hello.ts
+++ api/hello.ts
@@ -1,10 +1,12 @@
 // api/hello.ts
 import { NowRequest, NowResponse } from "@vercel/node";
 
+let count = 0;
+
 export default function (req: NowRequest, res: NowResponse) {
   console.log("req:", Object.keys(req));
   console.log("typeof req.body", typeof req.body)
-  console.log(`typeof req.body === "object" ?? Object.keys(req.body)`, typeof req.body === "object" ?? Object.keys(req.body))
+  console.log(`typeof req.body === "object" ?? Object.keys(req.body)`, typeof req.body === "object" && Object.keys(req.body))
   const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+  res.send(`Hello ${name}! ${++count}`);
 }
