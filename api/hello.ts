29f28a6e485940687d6670adeea26f8b544927bd
tamaritamari <shuhei.tamari@cureapp.jp>
2023-02-03 23:29:39 +0900

    test

--- api/hello.ts
+++ api/hello.ts
@@ -4,6 +4,7 @@ import { NowRequest, NowResponse } from "@vercel/node";
 export default function (req: NowRequest, res: NowResponse) {
   console.log("req:", Object.keys(req));
   console.log("typeof req.body", typeof req.body)
+  console.log(`typeof req.body === "object" ?? Object.keys(req.body)`, typeof req.body === "object" ?? Object.keys(req.body))
   const { name = "World" } = req.query;
   res.send(`Hello ${name}!`);
 }
