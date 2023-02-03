af6b4cdab8ecf6f116d07822e01cac2c7adb3349
tamaritamari <shuhei.tamari@cureapp.jp>
2023-02-03 23:10:38 +0900

    logger

--- api/hello.ts
+++ api/hello.ts
@@ -2,6 +2,8 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default function (req: NowRequest, res: NowResponse) {
+  console.log(Object.keys(req));
+  console.log(Object.keys(req.body))
   const { name = "World" } = req.query;
   res.send(`Hello ${name}!`);
 }
